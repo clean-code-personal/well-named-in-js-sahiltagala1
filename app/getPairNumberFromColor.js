18574a5b69650d7026f959fcdfa851ce7380d118
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-25 22:55:52 +0530

    Update getPairNumberFromColor.js

--- app/getPairNumberFromColor.js
+++ app/getPairNumberFromColor.js
@@ -1,11 +1,10 @@
-const ColorPair_file = require("./ColorPair");
-// import MajorColorNames from "./colorCoders";
-// import MinorColorNames from "./colorCoders";
-var MajorColorNames = ["WHITE", "RED", "BLACK", "YELLOW", "VIOLET"];
-var MinorColorNames = ["BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"];
+const { MajorColorNames, MinorColorNames, majorLen, minorLen } = require("./colors.js");
+const ColorPair_file = require("./ColorPair.js");
+
+
 function getPairNumberFromColor(pair) {
   let majorIndex = -1;
-  for (let i = 0; i < MajorColorNames.length; i++) {
+  for (let i = 0; i < majorLen; i++) {
     if (MajorColorNames[i] == pair.majorColor) {
       majorIndex = i;
       break;
@@ -13,7 +12,7 @@ function getPairNumberFromColor(pair) {
   }
 
   let minorIndex = -1;
-  for (let i = 0; i < MinorColorNames.length; i++) {
+  for (let i = 0; i < minorLen; i++) {
     if (MinorColorNames[i] == pair.minorColor) {
       minorIndex = i;
       break;
@@ -24,12 +23,12 @@ function getPairNumberFromColor(pair) {
     throw `Unknown Colors:${pair.toString()}`;
   }
 
-  return majorIndex * MinorColorNames.length + (minorIndex + 1);
+  return majorIndex * minorLen + (minorIndex + 1);
 }
 
 module.exports = new export_file_getPairNumberFromColor();
 function export_file_getPairNumberFromColor() {
   return {
-    getPairNumberFromColor:getPairNumberFromColor
+    getPairNumberFromColor: getPairNumberFromColor,
   };
-};
+}
