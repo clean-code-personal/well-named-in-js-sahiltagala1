04707c0d9ad0b5e5136d3c3c88a4834608a7a1b7
Sahil Tagala <60920150+sahiltagala1@users.noreply.github.com>
2023-02-25 22:57:03 +0530

    Update and rename colorCoder.js to main.js

---
+++ app/main.js
@@ -0,0 +1,6 @@
+const test_file = require('./test.js');
+const colorCodingPrint_file = require('./colorCodingPrint.js');
+
+test_file.test();
+console.log('Numbers : ColorName')
+colorCodingPrint_file.colorCodingPrint();
